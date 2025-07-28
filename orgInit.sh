8f4ed3a919faea6e3687e751efa3b9c2ccbe60fa
Jonathan Zhou <jonathan.zhou@jonathan-ltmcf4y.internal.salesforce.com>
2025-07-27 23:28:25 -0700

    start

---
+++ orgInit.sh
@@ -0,0 +1,21 @@
+# sf org create scratch -f config/project-scratch-def.json
+sf demoutil org create scratch -f config/project-scratch-def.json -d 30 -s -p dream -e house.workshop
+
+sfdx shane:tsp:username:update
+
+npm install
+node run.js
+sleep 120
+
+sf project deploy start
+sf org assign permset -n dreamhouse
+sf org assign permset -n Walkthroughs
+sf org assign permset -n EinsteinGPTPromptTemplateManager
+
+sf project deploy start -d prompt-app
+
+sf data import tree -p data/sample-data-plan.json
+
+sf demoutil user password set -p salesforce1 -g User -l User
+
+sf org open
